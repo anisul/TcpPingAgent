f3dd7f1cd0e6c24cfd0aa6c1f46738bef3dff675
anisul <anis.2803@gmail.com>
2018-10-17 19:51:06 +0200

    Functional revamp

--- src/TcpPingAgent.java
+++ src/TcpPingAgent.java
@@ -1,123 +1,64 @@
-import jade.core.AID;
-import jade.core.Agent;
-import jade.core.behaviours.SimpleBehaviour;
-import jade.lang.acl.ACLMessage;
-import jade.lang.acl.StringACLCodec;
-
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
 
+import jade.core.Agent;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+
 public class TcpPingAgent extends Agent {
-    private static final int TCP_SERVER_PORT = 1234;
-    ServerSocket server = null;
-    Socket socket = null;
+    Socket server = null;
+    String targethost = "35.180.88.76";
     BufferedReader in;
     PrintWriter out;
-    int value = 0;
-
-    //Call to Agent constructor
-    public TcpPingAgent(){
-        super();
-    }
+    int port = 1234;
 
     protected void setup() {
-        System.out.println("INFO:\t Agent started.");
-        try {
-            //Create server socket
-            server = new ServerSocket(TCP_SERVER_PORT);
-            socket = server.accept();
-            System.out.println("INFO:\t Connection to TCP server established.");
+        //Take host and port from argument
+        //Object[] args = getArguments();
+        //targethost = (String) args[0];
+        //port = Integer.parseInt((String) args[1]);
 
-            // Create writer and reader to send and receive data
-            out = new PrintWriter(socket.getOutputStream(), true);
-            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        } catch (IOException e) {
-            e.printStackTrace();
+        //Define agent
+        DFAgentDescription dfd = new DFAgentDescription();
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("Smith");
+        sd.setName(getName());
+        sd.addOntologies("SmithAgent");
+        dfd.setName(getAID());
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        } catch (FIPAException e) {
+            System.err.println(getLocalName()
+                    + " registration with DF did not succeed. Reason: "
+                    + e.getMessage());
+            doDelete();
         }
+        addBehaviour(new SendRequest());
     }
 
-    class TcpCommunicator extends SimpleBehaviour {
-
+    public class SendRequest extends OneShotBehaviour {
         @Override
         public void action() {
-            //TODO: Send message and receive message
-            String messageContentTest = "" + value;
-            System.out.println("Message sent to TCP Server: " + messageContentTest);
+            try {
+                //Open socket to TCP server
+                System.out.println("INFO:\t Opening socket to "+ targethost +":"+ port);
+                server = new Socket(targethost, port);
+                System.out.println("INFO:\t Successfully opened socket to "+ targethost +":"+ port);
 
-            // Get the answer and display it
-            String msgReceived;
-            msgReceived = callTcpServer(messageContentTest);
-            System.out.println("Message received from TCP Server: " + msgReceived);
-
-            // Increment the test variable
-            value++;
-        }
-
-        @Override
-        public boolean done() {
-            return false;
-        }
-    }
-
-    @Override
-    protected void takeDown() {
-        System.out.println("INFO:\t Agent being taken down.");
-        //Close writer and socket
-        try {
-            out.close();
-            in.close();
-            socket.close();
-            server.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //super.takeDown();
-    }
-
-    public String callTcpServer(String messageContent) {
-        ACLMessage message;
-        String tcpAnswer = "";
-
-        while (!tcpAnswer.equals(messageContent)) {
-            String acknowledgement = "";
-
-            while (!acknowledgement.equals("ok")) {
-                // Send Message to TCP server
-                message = new ACLMessage(ACLMessage.INFORM);
-                message.addReceiver(new AID("localhost:" + TCP_SERVER_PORT, AID.ISGUID));
-                message.setContent(messageContent);
-
-                // Encode message to send as an ACL Message
-                StringACLCodec codec = new StringACLCodec(in, out);
-                codec.write(message);
+                //write on the output stream (count of element in fibonacci series)
+                out = new PrintWriter(server.getOutputStream(), true);
+                out.println(Integer.parseInt("105"));
                 out.flush();
-
-                // Wait for ACK message
-                try
-                {
-                    acknowledgement = in.readLine().toString();
-                    in.reset();
-                    System.out.println("ack = " + acknowledgement);
-                } catch (IOException e) {
-                    //TODO: handle exception
-                }
-
-                // Wait for its answer
-                try
-                {
-                    while (!in.ready()) {}
-                    tcpAnswer = tcpAnswer + in.readLine().toString();
-                }
-                catch (IOException e)
-                {
-                    e.printStackTrace();
-                }
+                System.out.println("INFO:\t Successfully communicated with TCP server at "+ targethost +":"+ port);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-        return tcpAnswer;
     }
 }
